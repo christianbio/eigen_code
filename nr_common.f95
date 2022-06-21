42765df5f0b30f176e799b44b29b011868b96109
AWS ParallelCluster user <ec2-user@ip-10-0-12-230.eu-west-1.compute.internal>
2022-06-21 15:46:53 +0000

    numerical recipes common file

---
+++ nr_common.f95
@@ -0,0 +1,7 @@
+      module nr_common_data
+      real(8) :: linmin_param
+      real(8) :: pifac
+      integer nprintdata
+      integer frprmn_itmax
+      logical print_energies
+      end module nr_common_data
